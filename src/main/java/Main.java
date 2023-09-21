3224bfd3bb4e7ef0ec8d0ec3559988c8b158699a
Усер <oxxxyrniron@gmail.com>
2023-09-21 14:54:22 +0300

    Lab1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,32 @@
+
 public class Main {
     public static void main(String[] args){
         try{
-            Employee emp=new Employee("Bilbo Baggins");
+            Employee emp=new Employee("Bilbo Baggins", 67.86);
             emp.PrintEmpoyee();
         }catch(FieldLengthLimitException ex){
-            System.out.println(ex.getMessage());
+            System.out.println("User exception" + ex.getMessage());
+        }
+        catch (IncorrectSalaryException ex){
+            System.out.println("User exception" + ex.getMessage());
         }
         try{
-            Employee emp=new Employee("Bilbo Baggins from Shire");
+            Employee emp=new Employee("Bilbo Baggins from Shire", 41.50);
             emp.PrintEmpoyee();
-        }catch(FieldLengthLimitException ex){
-            System.out.println(ex.getMessage());
+        } catch(FieldLengthLimitException ex){
+            System.out.println("User exception" + ex.getMessage());
+        }
+        catch (IncorrectSalaryException ex){
+            System.out.println("User exception" + ex.getMessage());
         }
         try{
-            Employee emp=new Employee("Frodo Baggins");
+            Employee emp=new Employee("Frodo Baggins", -21.50);
             emp.PrintEmpoyee();
-        }catch(FieldLengthLimitException ex){
-            System.out.println(ex.getMessage());
+        } catch(FieldLengthLimitException ex){
+            System.out.println("User exception" + ex.getMessage());
+        }
+        catch (IncorrectSalaryException ex){
+            System.out.println("User exception" + ex.getMessage());
         }
     }
 }
